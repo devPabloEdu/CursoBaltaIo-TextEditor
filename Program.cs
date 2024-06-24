265cfb286f55a138977c8a4df3a1f94a3bb3646c
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-24 19:06:34 -0300

    criando a estrutura inicial do menu do programa

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void Menu(){
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - ABRIR ARQUIVO");
+            Console.WriteLine("2 - CRIAR NOVO ARQUIVO");
+            Console.WriteLine("0 - SAIR");
+            short option = short.Parse(Console.ReadLine());
+
+            switch(option){
+                case 0 : System.Environment.Exit(0);
+                break;
+                case 1 : Abrir();
+                break;
+                case 2 : Editar();
+                break;
+            }
+        }
+
+        static void Abrir(){
+
+        }
+        static void Editar(){
+
+        }
+    }
+}
