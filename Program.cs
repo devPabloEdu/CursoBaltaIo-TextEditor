c7d7036f561426d9ac628ad418b7f5e9e1372f23
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-24 19:48:42 -0300

    criando o metodo de abrir um arquivo

--- Program.cs
+++ Program.cs
@@ -31,7 +31,18 @@ namespace TextEditor
         }
 
         static void Abrir(){
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo ? ");
+            string path = Console.ReadLine();
 
+            using (var file = new StreamReader(path))
+            {
+              string text = file.ReadToEnd();  
+              Console.WriteLine(text);
+            }
+            Console.WriteLine(' ');
+            Console.ReadLine();
+            Menu();
         }
         static void Editar(){
             Console.Clear();
